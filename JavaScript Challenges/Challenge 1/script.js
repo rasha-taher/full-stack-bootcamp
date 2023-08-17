db9c425d5dc7d664a17de65f100d2046ff94b32d
rasha <rashataher1245@gmail.com>
2023-08-17 10:47:21 +0300

    Challenge 1 - Done with Step 6

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -11,9 +11,51 @@ console.log(age);
 console.log(lastName);
 console.log(DOB);
 age=22;
-lastName="Not Taher";
+// lastName="Not Taher";
 DOB= "7/8/01";
 console.log(age);
 console.log(lastName);
 console.log(DOB);
 
+//step 4
+let text="This is a text";
+let bool1= true;
+let bool2 =false;
+let number =1215;
+let string_number="999"
+let something={};
+let array=[];
+let nothing= null;
+let idk="undefined";
+
+console.log(text);
+console.log(bool1);
+console.log(bool2);
+console.log(number);
+console.log(string_number);
+console.log(something);
+console.log(array);
+console.log(nothing);
+console.log(idk);
+
+
+//step 5
+var myText=" string of variable";
+let var1=" double quotation";
+let var2 ='single quotation';
+let var3= `backticks`;
+const counter = 3;
+const backtick = `This is my text using backticks ${counter} times`;
+const singleQuote ='This is my text using single quotations ${counter} times';
+const doubleQuote ='This is my text using double quotations ${counter} times';
+const tryThisAlso = `This is my new version, it’s the ${2 + 3}th version`;
+console.log(myText);
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(counter);
+console.log(backtick);
+console.log(singleQuote);
+console.log(doubleQuote)
+console.log(tryThisAlso);
+console.log("Single and double quotes are used for regular strings.Backticks offer string interpolation, multi-line strings, and expression evaluation");
