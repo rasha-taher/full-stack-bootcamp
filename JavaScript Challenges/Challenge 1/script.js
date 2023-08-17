3ec0e5137b39fa65ebe226773faa235f5a9d9abf
rasha <rashataher1245@gmail.com>
2023-08-17 10:15:53 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,3 +1,19 @@
 console.log('I am linked now!');
+//step 1
  var name ="Rasha";
  alert(name);
+
+ //step 2
+var age =21;
+const lastName="Taher";
+let DOB="07/08/2001";
+console.log(age);
+console.log(lastName);
+console.log(DOB);
+age=22;
+lastName="Not Taher";
+DOB= "7/8/01";
+console.log(age);
+console.log(lastName);
+console.log(DOB);
+
