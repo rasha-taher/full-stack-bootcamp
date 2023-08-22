9d2ddbe97fa5d7bad5e180d2139c2b351b0fb53d
rasha <rashataher1245@gmail.com>
2023-08-22 14:51:23 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -8,3 +8,19 @@ let personalInfo={
     location: "Beirut, Lebanon",
     nationality : "Lebanese"
 };
+
+//step 2 
+let nextThreeNumbers=[5 , 1, 10];
+let allNumbers ;
+allNumbers=[...fiveNumbers, ...nextThreeNumbers];
+console.log(allNumbers);
+
+let additionalInfo={
+    occupation : "web developer",
+    hobby: "watching movie/series",
+    education : "computer science"
+}
+
+let fullInfo= {...personalInfo, ...additionalInfo};
+console.log(fullInfo);
+
