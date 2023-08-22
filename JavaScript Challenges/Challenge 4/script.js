45354bfca827a0b0406a7e837349e9e114a413c9
rasha <rashataher1245@gmail.com>
2023-08-22 14:50:52 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,10 @@
+//step 1 
+let fiveNumbers=[2, 7, 12, 50, 3];
+
+let personalInfo={
+    name: "Rasha",
+    age : 22,
+    height:165,
+    location: "Beirut, Lebanon",
+    nationality : "Lebanese"
+}
