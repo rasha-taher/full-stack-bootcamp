4870c4099ab10f22ceaf6691b519848cabf47544
rasha <rashataher1245@gmail.com>
2023-08-24 15:58:20 +0300

    Challenge 5.1 - Done with step 3

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -28,7 +28,7 @@ document.addEventListener('keydown', function(event) {
     if (event.key === 'a' || event.key === 'A') {
       htext.style.backgroundColor = 'red';
     } else if (event.key === 's' || event.key === 'S') {
-      const currentLeft = parseInt(getComputedStyle(htext).left) || 0;
+      const currentLeft = parseInt(getComputedStyle(htext).left);
       htext.style.left = (currentLeft + 10) + 'px';
     }
     else if(event.key==='d'|| event.key==='D'){
@@ -49,6 +49,67 @@ document.addEventListener('keydown', function(event) {
       }
   });
   
-  
-  
-  
+  //step 3
+  let form = document.createElement('form');
+  document.body.appendChild(form);
+
+  let fullName =document.createElement('input')
+  fullName.type="text"
+  fullName.placeholder="Full Name"
+
+  let email =document.createElement('input')
+  email.type="email"
+  email.placeholder="Email"
+
+  let password =document.createElement('input')
+  password.type="password"
+  password.placeholder="Password"
+
+  let confpassword =document.createElement('input')
+  confpassword.type="password"
+  confpassword.placeholder="Confirm Password"
+
+  let subButton =document.createElement('input')
+  subButton.type="button";
+    subButton.value="Submit"
+
+  form.appendChild(fullName)
+  form.appendChild(email)
+  form.appendChild(password)
+  form.appendChild(confpassword)
+  form.appendChild(subButton)
+
+subButton.addEventListener("click", function (event) {
+   event.preventDefault();
+   var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if(! pattern.test(email.value)){
+        email.setAttribute("placeholder", "Please enter a valid email address.");
+        email.style.borderColor = "red"
+        return false;
+    }else{
+    email.style.borderColor="gray";
+    return true;
+    }
+ });  
+
+ 
+ subButton.addEventListener("click", function (event) {
+    event.preventDefault();
+ if(password.value!==confpassword.value){
+    password.setAttribute("placeholder", "Passwords don't match");
+     confpassword.setAttribute("placeholder", "Passwords don't match");
+     alert("Passwoords don't match ")
+ }
+});
+
+let inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  input.addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'red';
+  });
+
+  input.addEventListener('mouseout', function(event) {
+    event.target.style.backgroundColor = ''; 
+  });
+});
