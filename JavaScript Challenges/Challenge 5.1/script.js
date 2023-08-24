f07bfc9f8f0d56e3aca47d3f310713858311470d
rasha <rashataher1245@gmail.com>
2023-08-24 15:14:51 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -16,3 +16,39 @@ button.addEventListener('click', function(){
     this.style.color="white";
     this.textContent=`Clicked ${randomNumber}`
 })
+
+// step 2 
+
+let htext = document.createElement('h1')
+document.body.appendChild(htext);
+htext.style.border="10px dotted green"
+htext.textContent="some random text"
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'a' || event.key === 'A') {
+      htext.style.backgroundColor = 'red';
+    } else if (event.key === 's' || event.key === 'S') {
+      const currentLeft = parseInt(getComputedStyle(htext).left) || 0;
+      htext.style.left = (currentLeft + 10) + 'px';
+    }
+    else if(event.key==='d'|| event.key==='D'){
+        let ptext = document.createElement('p')
+        document.body.appendChild(ptext)
+        ptext.textContent="Key D was pressed"
+    }
+    else if(event.key==='w'|| event.key==='W'){
+        if(htext.style.display=="none"){
+            htext.style.display="block"
+        }else {
+            htext.style.display="none"
+        }
+    }
+    else if (event.key === ' ' || event.key === 'Spacebar') {
+        const currentFontSize = parseInt(getComputedStyle(htext).fontSize);
+        htext.style.fontSize = (currentFontSize + 5) + 'px';
+      }
+  });
+  
+  
+  
+  
